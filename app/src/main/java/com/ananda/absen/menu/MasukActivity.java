a43b6f0ffe278cca374c2547310b4bb5288470fc
Adi Riyanto <siyaga@users.noreply.github.com>
2020-07-16 15:18:51 +0700

    Desin Main Menu

---
+++ app/src/main/java/com/ananda/absen/menu/MasukActivity.java
@@ -0,0 +1,16 @@
+package com.ananda.absen.menu;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.ananda.absen.R;
+
+public class MasukActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_masuk);
+    }
+}
